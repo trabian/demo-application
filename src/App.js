70a74f60e16c9a9288f43fdb0d6d2138e2576e67
Jimmy Miller <jimmy@trabian.com>
2017-05-30 15:08:17 -0400

    added background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
+import { Flex, Inline, curry } from 'jsxstyle';
+
+
+const Container = curry(Flex, {
+  padding: 30,
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  height: "100vh",
+  width: "80vw",
+  margin: "0 auto",
+})
+
+const Heading = curry(Inline, {
+  color: '#fff',
+  fontSize: '3em',
+})
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
-      </div>
+      <Container>
+        <Flex>
+          <Heading color="#fff">Member Application</Heading>
+        </Flex>
+      </Container>
     );
   }
 }
