e3842e7b8be1febad7ed8591c84f3ec8320ea48c
Clayton Marshall <cmarshall@Claytons-MacBook-Pro.local>
2017-05-31 14:33:35 -0400

    ProductCards displaying correctly. Still need icons on selection buttons

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import { Flex, Inline, curry} from 'jsxstyle';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import ProductCard from './components/ProductCard.js';
-
+import savingsImage from './components/images/savings.png';
+import cardImage from './components/images/card.png';
+import spendingImage from './components/images/spending.png';
 
 const Container = curry(Flex, {
-  padding: 30,
+  paddingTop: 30,
   flexDirection: "column",
   justifyContent: "flex-start",
   alignItems: "flex-start",
-  height: "100vh",
   width: "80vw",
   margin: "0 auto",
-})
+});
 
 const Heading = curry(Inline, {
   color: '#fff',
   fontSize: '2.8em',
   alignSelf: 'flex-start'
-})
+});
 
 const FlexRow = curry(Flex, {
   flexDirection: 'row'
-})
+});
 
 class App extends Component {
   render() {
@@ -33,29 +34,27 @@ class App extends Component {
         <Flex>
           <Heading color="#fff">Member Application</Heading>
         </Flex>
-        <Flex style={{ alignSelf: 'center', flexDirection: 'column'}}>
-          <Flex style={{ flexDirection: 'column', alignItems: 'center', color: 'white' }}>
+        <Flex style={{ alignSelf: 'center', flexDirection: 'column', alignItems: 'center', color: 'white'}}>
             <b style={{ fontSize: '1.5em' }}>Select your products</b>
             <p>Which products would you like?</p>
-          </Flex>
         </Flex>
         <FlexRow style={{ alignSelf: 'center' }}>
           <MuiThemeProvider>
-            <ProductCard img="/public/images/savings.png" title="Savings Account"
+            <ProductCard img={savingsImage} title="Savings Account"
             subText="Your membership begins with a $20 deposit into a savings account. This account also pays dividents."
-            buttonText="button" buttonColor="#3d917b" backgroundColor="white"
+            buttonText="Add To My Selection" buttonColor="#3d917b" backgroundColor="white"
             />
           </MuiThemeProvider>
           <MuiThemeProvider>
-            <ProductCard img="" title="Spending Account"
+            <ProductCard img={cardImage} title="Spending Account"
             subText="Your spending account is what your debit card, paper checks and other expenditures will draw from. You may also use this account for your direct deposit."
-            buttonText="button" buttonColor="#64ac31" backgroundColor="white"
-          />
+            buttonText="Selected" buttonColor="#64ac31" backgroundColor="white"
+            />
           </MuiThemeProvider>
           <MuiThemeProvider>
-            <ProductCard img="" title="Credit Card"
+            <ProductCard img={spendingImage} title="Credit Card"
             subText="We focus on keeping our credit card rates fair and costs low. If you already have a card, we may be able to help you reduce your payments."
-            buttonText="button" buttonColor="#d40f61" backgroundColor="white"/>
+            buttonText="Remove" buttonColor="#d40f61" backgroundColor="white"/>
           </MuiThemeProvider>
         </FlexRow>
       </Container>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,11 +3,12 @@ import { RaisedButton } from 'material-ui';
 import { Col, Flex } from 'jsxstyle';
 
 
-const flexstyle = {
+const buttonText = {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
+  color: '#fff'
 };
 
 const buttonStyle = {
@@ -15,23 +16,34 @@ const buttonStyle = {
   marginBottom: '10px'
 };
 
+const wrapperStyles = {
+  padding: 20,
+  paddingBottom:10,
+  width: 250,
+  height: 360,
+  marginRight: '2vw',
+  marginLeft: '2vw'
+};
+
 const Product_Card = (props)=>{
   return(
-    <Col alignItems='center' justifyContent='space-between' style={{paddingLeft: '20px',paddingRight: '20px', width: '250px', height: '350px', backgroundColor: props.backgroundColor, marginRight: '2vw', marginLeft: '2vw' }}>
-      <Flex style={{ flexDirection:'column', alignItems: 'center'}}>
-        <img src={props.img} alt="Insert image here" width="100px" height="100px"/>
-        <h2>{props.title}</h2>
+    <Col alignItems='center' style={{...wrapperStyles, backgroundColor: props.backgroundColor}}>
+      <Flex style={{ flexDirection:'column', alignItems: 'center' }}>
+        <img src={props.img} height="95px" alt="Insert image here" style={{ marginTop: '10px' }}/>
       </Flex>
-      <Flex style={{ flexDirection: 'column', alignItems: 'center' }}>
-        <p>{props.subText}</p>
+
+      <Flex style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', height: 200}}>
+        <br />
+        <h3 style={{color: '#3c3c3c'}}>{props.title}</h3>
+        <p style={{color: '#707275', fontSize: '0.9em', margin: 0, padding: 0}}>{props.subText}</p>
       </Flex>
+
       <RaisedButton
         fullWidth={true}
         backgroundColor={props.buttonColor}
-        labelColor="white"
         style={buttonStyle}
       >
-          <div style={{ width: '100%', height: '100%', ...flexstyle}}>
+          <div style={{ width: '100%', height: '100%', ...buttonText}}>
            {props.buttonText}
           </div>
       </RaisedButton>
