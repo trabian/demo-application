3d096a0000000b0e0903b0e0c149dae6331a9856
Clayton Marshall <cmarshall@Claytons-MBP.attlocal.net>
2017-06-01 12:58:50 -0400

    Accidentally saved files from redux-card branch to this branch. Should be fixed now.

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-import { select } from '../action_types.js';
-export const select = (cardTitle)=>{
-  return {
-    type: select,
-    payload: cardTitle
-  };
-};
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import selection_reducer from './selection_reducer.js';
-
-export default combineReducers({
-  selected: selection_reducer
-});
