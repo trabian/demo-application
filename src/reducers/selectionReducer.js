ce16e6483c06782785ccf78eb84973324734c22c
Jimmy Miller <jimmy@trabian.com>
2017-06-07 13:53:10 -0400

    Refactored state machine

--- src/reducers/selectionReducer.js
+++ src/reducers/selectionReducer.js
@@ -1,8 +1,33 @@
+import { getIn, set } from 'zaphod/compat';
+
 import { SELECT, HOVER_IN, HOVER_OUT } from '../actions';
 
-export const UNSELECTED = 0;
-export const SELECTED = 1;
-export const HOVERED = 2;
+export const UNSELECTED = 'UNSELECTED';
+export const SELECTED = 'SELECTED';
+export const HOVERED = 'HOVERED';
+
+
+// This is a state machine.
+// First level is the action.
+// Then it is the current state.
+// The final value is the new state
+const stateTransitions = {
+  [SELECT]: {
+    [UNSELECTED]: SELECTED,
+    [SELECTED]: UNSELECTED,
+    [HOVERED]: UNSELECTED,
+  },
+  [HOVER_IN]: {
+    [UNSELECTED]: UNSELECTED,
+    [SELECTED] : HOVERED,
+    [HOVERED]: HOVERED,
+  },
+  [HOVER_OUT]: {
+    [UNSELECTED]: UNSELECTED,
+    [HOVERED]: SELECTED,
+    [SELECTED]: SELECTED,
+  }
+}
 
 const initialState = {
   savings: UNSELECTED,
@@ -10,49 +35,8 @@ const initialState = {
   credit: UNSELECTED,
 };
 
-const selectTransformations = {
-  [UNSELECTED]: SELECTED,
-  [SELECTED]: UNSELECTED,
-  [HOVERED]: UNSELECTED,
-};
-
-const hoverOutTransformations = {
-  [HOVERED]: SELECTED,
-  [UNSELECTED]: UNSELECTED,
-  [SELECTED]: SELECTED
-};
-
-const hoverInTransformations = {
-  [UNSELECTED]: UNSELECTED,
-  [SELECTED] : HOVERED,
-  [HOVERED]:HOVERED
-};
-
-const selectHandler = (state, cardId) => {
-  const newState = selectTransformations[state[cardId]];
-  return { [cardId]: newState };
-};
-
-const hoverInHandler = (state, cardId) => {
-  const newState = hoverInTransformations[state[cardId]];
-  return { [cardId]: newState };
-};
-
-const hoverOutHandler = (state, cardId) => {
-  const newState = hoverOutTransformations[state[cardId]];
-  return { [cardId]: newState };
-};
-
-const actionHandlers = {
-  [SELECT]: selectHandler,
-  [HOVER_IN]: hoverInHandler,
-  [HOVER_OUT]: hoverOutHandler
-};
-
-export default (state=initialState, action) => {
-  const handlerFunction = actionHandlers[action.type];
-  if(!handlerFunction){
-    return state;
-  }
-  return {...state, ...handlerFunction(state,action.id)};
+export default (state=initialState, { id: cardId, type }) => {
+  const currentCardState = state[cardId];
+  const newCardState = getIn(stateTransitions, [type, currentCardState], state)
+  return set(state, cardId, newCardState);
 };
