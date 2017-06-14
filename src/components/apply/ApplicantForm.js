9692a0b67a97c4d5fec8a28a5aceed5d92abe5c8
Casey Primozic <me@ameo.link>
2017-06-14 10:26:23 -0400

    Initial redux-forms integration + skeleton 'Apply' form
     * Pulled in `redux-form` as a dependency and created a basic `ApplicantForm` that's exported with a `reduxForm()` wrapper
     * Set the `Apply.js` file to simply contain the `<ApplicantForm>` for now.

---
+++ src/components/apply/ApplicantForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { reduxForm } from 'redux-form';
+
+// Will be added in the future
+// import IdentificationForm from './IdentificationForm';
+// import ContactInfo from './ContactInfo';
+// import ContinueButton from '../ContinueButton';
+
+const headingStyle = {
+  fontSize: '18pt',
+  color: '#fff',
+  textAlign: 'center',
+  marginBottom: 10,
+  marginTop: 22,
+  fontWeight: 'bold'
+};
+
+const SectionHeading = ({ children }) => <div style={headingStyle}>{children}</div>;
+
+const ApplicantForm = ({ handleSubmit }) => (
+  <div style={{marginBottom: 12}}>
+    <form onSubmit={handleSubmit}>
+      <SectionHeading>Your Identity</SectionHeading>
+      {/* <IdentificationForm /> */}
+      <br />
+
+      <SectionHeading>Contact Information and Address</SectionHeading>
+      {/* <ContactInfo />
+      <center><ContinueButton title='KEEP GOING' buttonProps={{type: 'submit'}} /></center> */}
+    </form>
+  </div>
+);
+
+export default reduxForm({form: 'apply'})(ApplicantForm);
--- src/components/apply/Apply.js
+++ src/components/apply/Apply.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Apply = () => { return <div>TODO</div>; }
+import ApplicantForm from './ApplicantForm';
+
+const Apply = () => (
+  <div>
+    <ApplicantForm />
+  </div>
+);
 
 export default Apply;
