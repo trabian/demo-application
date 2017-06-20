94737160d704c48da924db1a36720ec09a432a2f
marsha88 <marsha88@purdue.edu>
2017-06-20 09:24:13 -0400

    Moved all validation to formValidators.js

--- src/components/apply/ApplicantForm.js
+++ src/components/apply/ApplicantForm.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
-import { required, email } from 'redux-form-validators';
 // Will be added in the future
 import IdentificationForm from 'src/components/apply/IdentificationForm';
 import ContactInfo from 'src/components/apply/ContactInfo';
 import ContinueButton from 'src/components/ContinueButton';
+import { validate } from 'src/components/apply/formValidators';
 
 const headingStyle = {
   fontSize: '18pt',
@@ -14,35 +14,6 @@ const headingStyle = {
   marginTop: 22,
   fontWeight: 'bold'
 };
-
-const requiredInput = required({msg: 'Required'});
-const validations = {
-  'firstName': [requiredInput],
-  'lastName':[requiredInput],
-  'middleInitial': [requiredInput],
-  'soc':[requiredInput],
-  'dob':[requiredInput],
-  'phoneNumber': [requiredInput],
-  'phoneNumberType':[requiredInput],
-  'emailAddress':[requiredInput, email({msg: 'Inavlid Email'})],
-  'address':[requiredInput],
-  'city': [requiredInput],
-  'state':[requiredInput],
-  zipCode:[requiredInput]
-};
-
-const validate = (values) => {
-  const errors = {}
-  for (let field in validations) {
-    let value = values[field]
-    errors[field] = validations[field].map(validateField => {
-      return validateField(value, values)
-    }).find(x => x)
-  }
-  return errors
-}
-
-
 
 const SectionHeading = ({ children }) => <div style={headingStyle}>{children}</div>;
 
--- src/components/apply/formValidators.js
+++ src/components/apply/formValidators.js
@@ -1,28 +1,34 @@
-import { required, numericality, length, email } from 'redux-form-validators';
-
+import { required, email } from 'redux-form-validators';
+/* consider writing own required and email validators to avoid using redux-form-validators all together */
+const requiredInput = required({msg: 'Required'});
 const validations = {
-  firstName: [required(), length({is: 2})],
-  lastName: [required()],
-  middleInitial:[required()],
-  soc: [required(), length({is: 11})],
-  phoneNumber: [required(), length({is: 14})],
-  phoneNumberType: [required()],
-  email:    [required(), email()],
-  dob:      [
-    required(),
-    numericality({ int: true }),
-    numericality({ '>=': 18, msg: 'Must be 18 years or older to apply.' })
-  ],
+  firstName: [requiredInput],
+  lastName:[requiredInput],
+  middleInitial: [requiredInput],
+  soc:[requiredInput],
+  dob:[requiredInput],
+  phoneNumber: [requiredInput],
+  phoneNumberType:[requiredInput],
+  emailAddress:[requiredInput, email({msg: 'Inavlid Email'})],
+  address:[requiredInput],
+  city: [requiredInput],
+  state:[requiredInput],
+  zipCode:[requiredInput]
 };
 
-/* this is the default function used in redux-validator-form library */
-export const validate = (values) => {
-  const errors = {}
+/* The for loop is boilerplate code from redux-form-validators
+   that runs redux-form inputs through their corresponding
+   validators and adds an error message to the errors
+   object if one of the validators fails. (returns
+   error message from first validator to error).
+*/
+export const validate = (values)=>{
+  const errors = {};
   for (let field in validations) {
     let value = values[field]
     errors[field] = validations[field].map(validateField => {
-      return validateField(value, values)
-    }).find(x => x)
+      return validateField(value,values)
+    }).find(x => x);
   }
   return errors;
-}
+};
