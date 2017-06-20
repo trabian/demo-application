5dc3a448fbe5d3d5a27381a088dcf488b1bbe873
Casey Primozic <me@ameo.link>
2017-06-20 09:49:58 -0400

    Converted the `validate()` function into a functional equivalent
     * Basically just a reducer function that maps the map of validator arrays over the input and returns all that reject.

--- src/components/apply/formValidators.js
+++ src/components/apply/formValidators.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { required, email } from 'redux-form-validators';
 
@@ -22,19 +23,23 @@ const validations = {
   zipCode:[requiredInput]
 };
 
-/* The for loop is boilerplate code from redux-form-validators
+// Applies all the validator functions to the input, returning the value of the first rejection
+// or `undefined`.
+const applyValidators = (validators, value, values) => _.find(_.map(validators, validator => validator(value, values)));
+
+/* This function is adapted from code from redux-form-validators
    that runs redux-form inputs through their corresponding
    validators and adds an error message to the errors
    object if one of the validators fails. (returns
    error message from first validator to error).
 */
-export const validate = (values)=>{
-  const errors = {};
-  for (let field in validations) {
-    let value = values[field]
-    errors[field] = validations[field].map(validateField => {
-      return validateField(value,values)
-    }).find(x => x);
-  }
-  return errors;
+export const validate = values => {
+  return _.reduce(values, (acc, val, key) => {
+    const errorMsg = validations[key] && applyValidators(validations[key], val, values);
+    if(errorMsg) {
+      return {...acc, [key]: errorMsg};
+    } else {
+      return acc;
+    }
+  }, {});
 };
