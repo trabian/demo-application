246fa2ce6de190c062aee012af618ccc1dced2c0
Casey Primozic <me@ameo.link>
2017-06-23 13:53:24 -0400

    Changed redux-form state initialization to use redux-form functions
     * Rather than merging raw state values in, we use the actual field values and merge them into the form using `initialize()`
     * Only show the Trabian logo in the console if we're in development mode

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer, blur, focus } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
-import reduceReducers from 'reduce-reducers';
 import { getIn, updateIn } from 'zaphod/compat';
 
 import { transformFieldEntry } from 'src/reducers/formHelpers';
 import selectionReducer from 'src/reducers/selectionReducer';
-import { devToolsReducer } from 'src/utils/devTools';
 
 const blurType = blur().type;
 const focusType = focus().type;
@@ -20,12 +18,10 @@ const applyFormReducer = (state, action={}) => {
   return updateIn(state, ['values', fieldName], transformFieldEntry(fieldName, action.type));
 };
 
-const combinedReducers = combineReducers({
+export default combineReducers({
   selected: selectionReducer,
   router: routerReducer,
   form: formReducer.plugin({
     apply: applyFormReducer,
   }),
 });
-
-export default reduceReducers(combinedReducers, devToolsReducer);
--- src/utils/devTools.js
+++ src/utils/devTools.js
@@ -3,39 +3,31 @@
  * in both development and production.
  */
 
-import { merge } from 'zaphod/compat';
+import { initialize } from 'redux-form';
 
-import completedLoanApplication from 'src/data/completedApplication';
+import { applyFormValues } from 'src/data/completedApplication';
 
-const trabianText = `
-  ______           __    _               __
- /_  __/________ _/ /_  (_)___ _____   _/_/
-  / / / ___/ __ \`/ __ \\/ / __ \`/ __ \\_/_/
- / / / /  / /_/ / /_/ / / /_/ / / / < <
-/_/ /_/   \\__,_/_.___/_/\\__,_/_/ /_// /
-                                    \\_\\
+if(process.env.NODE_ENV === 'development') {
+  const trabianText = `
+    ______           __    _               __
+   /_  __/________ _/ /_  (_)___ _____   _/_/
+    / / / ___/ __ \`/ __ \\/ / __ \`/ __ \\_/_/
+   / / / /  / /_/ / /_/ / / /_/ / / / < <
+  /_/ /_/   \\__,_/_.___/_/\\__,_/_/ /_// /
+                                      \\_\\
 
-Access dev tools through the global \`trabian\` variable.
-`;
+  Access dev tools through the global \`trabian\` variable.
+  `;
 
-console.log(trabianText);
-
-const FILL_OUT_LOAN_APPLICATION = 'FILL_OUT_LOAN_APPLICATION';
-
-export const devToolsReducer = (state, action) => {
-  if(action.type === FILL_OUT_LOAN_APPLICATION) {
-    return merge(state, completedLoanApplication);
-  }
-
-  return state;
-};
+  console.log(trabianText);
+}
 
 // Function that takes one argument, the global Redux store, and returns an object designed to be injected into
 // the application's global scope and be made accessible to developers.
 const devTools = store => ({
   fillOutLoanApplication: () => {
-    // merge the completed application state into the current state
-    store.dispatch({type: FILL_OUT_LOAN_APPLICATION});
+    const initializeAction = initialize('apply', applyFormValues);
+    store.dispatch(initializeAction);
   },
 });
 
