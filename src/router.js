38af4b1dd8b37a1808a626c8a40a1ed9d966f6ff
marsha88 <marsha88@purdue.edu>
2017-06-23 14:43:55 -0400

    Added field array example that successfully submits similar to the application page form

--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ import { ConnectedRouter} from 'react-router-redux';
 import IndexPage from 'src/components/product_select/ProductSelect';
 import Apply from 'src/components/apply/Apply';
 import NotFound from 'src/components/NotFound';
-const Placeholder = () => (<p> Welcome to a placeholder page.</p>);
+
+import FieldArray from 'src/components/FieldArray';
 
 export const PRODUCT_SELECT = '/';
 export const APPLY = '/apply';
@@ -16,7 +17,7 @@ export const CoreRouter = ({history}) => (
     <Switch>
       <Route exact path={PRODUCT_SELECT} component={IndexPage} />
       <Route exact path={APPLY} component={Apply} />
-      <Route exact path={DISCLOSURES} component={Placeholder} />
+      <Route exact path={DISCLOSURES} component={FieldArray} />
       <Route path='*' component={NotFound} />
     </Switch>
   </ConnectedRouter>
