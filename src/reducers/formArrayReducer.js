c1cfe20f6dca738d055d3c16ec7ce33854fe8743
Casey Primozic <me@ameo.link>
2017-06-27 15:35:58 -0400

    Removed mutation in the joint applicant form reducer

--- src/reducers/formArrayReducer.js
+++ src/reducers/formArrayReducer.js
@@ -2,13 +2,13 @@ import { ADD_APPLICANT, REMOVE_APPLICANT, SELECT_APPLICANT } from 'src/actions';
 
 const initialState = {
   cursor: 0,
-  forms: [],
+  forms: [{}],
 };
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case ADD_APPLICANT:
-      return {...state, forms: state.forms.push(action.payload)};
+      return {...state, forms: [...state.forms, {}]};
     case REMOVE_APPLICANT:
       return {...state, forms: state.forms.splice(action.payload, 1)};
     case SELECT_APPLICANT:
