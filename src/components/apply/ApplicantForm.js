ae1808fe5e386ee4446c8c207d8138436c00fffb
marsha88 <marsha88@purdue.edu>
2017-06-27 11:04:40 -0400

    Merged in changes from joint-tabs branch. There is a strong chance of rewriting this entire FieldArray portion as doing form level validation with the fieldArrays make everything more complicated.

--- src/components/apply/ApplicantForm.js
+++ src/components/apply/ApplicantForm.js
@@ -43,87 +43,109 @@ const selector = formValueSelector('apply');
 const mapStateToProps = state => {
   const addJointApplicant = selector(state, 'addJointApplicant');
   return {
-    addJointApplicant
+    addJointApplicant,
+    selectedApplicantId: state.selectedApplicant.id
   };
 };
 
-const singleApplicationForm = ({ fields, addJointApplicant, meta }) => {
-  if( fields.length === 0 ){ fields.push({}); }
-
-  return (
-  <div>
-    {fields.map((member, index) => {
-        return(
-          <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}} key={index}>
-            <SectionHeading>Your Identity</SectionHeading>
-            <IdentificationForm member={member} />
-            <SectionHeading>Contact Information and Address</SectionHeading>
-            <ContactInfo member={member} />
-            <Flex alignSelf='center' width='45%' style={{marginLeft: 20, marginBottom: 0, marginTop: 30}}>
-              <Field
-                  name="addJointApplicant"
-                  component={Checkbox}
-                  label="Add joint applicant to my membership application"
-                  labelStyle={{color: colors.basic}}
-                  iconStyle={{fill: colors.basic, marginLeft: 10}}
-              />
-            </Flex>
-            <center>
-                <ContinueButton
-                  title='KEEP GOING'
-                  onClick={() => fields.push({})}
-                  buttonProps={{type: buttonType(addJointApplicant)}}
-                  style={{ marginTop: 10 }}
-                />
-            </center>
-          </div>
-        );
-    })}
-  </div>
-); };
-
-const singleAppArrayEntry = connect(mapStateToProps)(singleApplicationForm);
-
-const SectionHeading = ({ children }) => <div style={headingStyle}>{children}</div>;
-const buttonType = (addJointApplicant) => {
-  if(addJointApplicant){
-    return 'button';
-  }
-  return 'submit';
-};
-
-const ApplicantForm = ({selectedApplicantId, jointApplicantCount, setSelectedApplicant, setJointApplicantCount, handleSubmit, history}) => {
-  const applicantTabs = _.map(_.range(jointApplicantCount), i => {
-    return <Tab style={tabStyle} label={`Joint Applicant ${i + 1}`} key={i} value={i} />;
-  });
-  // Add an additional tab at the end to add additional joint applicants
-  const allTabs = [
-    ...applicantTabs,
-    <Tab style={tabStyle} label={'+ Add Joint Applicant'} key={applicantTabs.length} value={applicantTabs.length} />
-  ];
-
-  const handleTabClick = index => {
-    if(index === jointApplicantCount) {
-      // the "Add New Applicant" tab was clicked, so add a new applicant to the list
-      setJointApplicantCount(jointApplicantCount + 1);
+const singleApplicationForm = connect(mapStateToProps)(({ selectedApplicantId, fields, addJointApplicant }) => {
+    if(fields.length === 0 || fields.length === selectedApplicantId) {
+      fields.push({});
     }
-    setSelectedApplicant(index);
-  };
-  return(
-    <div style={{marginBottom: 12}}>
-      <form onSubmit={handleSubmit(formSubmit(history))}
-        style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Tabs
-          style={{width: '100%', marginTop: 26}}
-          onChange={handleTabClick}
-          value={selectedApplicantId}
-        >
-          {allTabs}
-        </Tabs>
-        <FieldArray name="applications" component={singleAppArrayEntry} />
-      </form>
-    </div>
-  );
-};
 
-export default withRouter(reduxForm({form: 'apply', validate})(ApplicantForm));
+    const fieldMapper = (member, memberIndex) => {
+      if(memberIndex !== selectedApplicantId) {
+        return null; // only display the currently selected applicant
+      }
+      return (
+        <div key={memberIndex}>
+          {fields.map((member, index) => {
+            return(
+              <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}} key={index}>
+                <SectionHeading>Your Identity</SectionHeading>
+                <IdentificationForm member={member} />
+                <SectionHeading>Contact Information and Address</SectionHeading>
+                <ContactInfo member={member} />
+                <Flex alignSelf='center' width='45%' style={{marginLeft: 20, marginBottom: 0, marginTop: 30}}>
+                  <Field
+                    name="addJointApplicant"
+                    component={Checkbox}
+                    label="Add joint applicant to my membership application"
+                    labelStyle={{color: colors.basic}}
+                    iconStyle={{fill: colors.basic, marginLeft: 10}}
+                  />
+                </Flex>
+                <center>
+                  <ContinueButton
+                    title='KEEP GOING'
+                    onClick={() => fields.push({})}
+                    buttonProps={{type: buttonType(addJointApplicant)}}
+                    style={{ marginTop: 10 }}
+                  />
+                </center>
+              </div>
+            );
+          })}
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        {fields.map(fieldMapper)}
+      </div>
+    );
+});
+
+      const SectionHeading = ({ children }) => <div style={headingStyle}>{children}</div>;
+      const buttonType = (addJointApplicant) => {
+        if(addJointApplicant){
+          return 'button';
+        }
+        return 'submit';
+      };
+
+      const ApplicantForm = ({selectedApplicantId, jointApplicantCount, setSelectedApplicant, setJointApplicantCount, handleSubmit, history}) => {
+        const applicantTabs = _.map(_.range(jointApplicantCount), i => {
+          return <Tab style={tabStyle} label={`Joint Applicant ${i + 1}`} key={i} value={i} />;
+        });
+        // Add an additional tab at the end to add additional joint applicants
+        const allTabs = [
+          ...applicantTabs,
+          <Tab style={tabStyle} label={'+ Add Joint Applicant'} key={applicantTabs.length} value={applicantTabs.length} />
+        ];
+
+        const handleTabClick = index => {
+          if(index === jointApplicantCount) {
+            // the "Add New Applicant" tab was clicked, so add a new applicant to the list
+            setJointApplicantCount(jointApplicantCount + 1);
+          }
+          setSelectedApplicant(index);
+        };
+        return(
+          <div style={{marginBottom: 12}}>
+            <form onSubmit={handleSubmit(formSubmit(history))}
+              style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <Tabs
+                style={{width: '100%', marginTop: 26}}
+                onChange={handleTabClick}
+                value={selectedApplicantId}
+                >
+                  {allTabs}
+                </Tabs>
+                <FieldArray name="applications" component={singleApplicationForm} />
+              </form>
+            </div>
+          );
+        };
+
+        const mapApplicantFormState = state => ({
+          selectedApplicantId: state.selectedApplicant.id,
+          jointApplicantCount: state.selectedApplicant.count,
+        });
+
+        export default connect(mapApplicantFormState, {setSelectedApplicant, setJointApplicantCount})(
+          withRouter(
+            reduxForm({form: 'apply', validate})(ApplicantForm)
+          )
+        );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,35 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer, blur, focus } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
+import _ from 'lodash';
 import { getIn, updateIn } from 'zaphod/compat';
 
 import { transformFieldEntry } from 'src/reducers/formHelpers';
 import selectionReducer from 'src/reducers/selectionReducer';
+import { selectedApplicantReducer } from 'src/reducers/selectedApplicant';
 
 const blurType = blur().type;
 const focusType = focus().type;
+
+/**
+ * Removes the dot-and-bracket notation from the name fields, returning the index of the field in the applicants `FieldArray`
+ * and the actual name of the field.
+ *
+ * For example, it converts 'applicants[0].firstName' into {index: 0, field: 'firstName'}.
+ */
+const stripFieldNamePrefix = name => ({
+  field: _.last(_.split(name, '.')),
+  index: _.split(name, /[[\]]/g)[1],
+});
 
 const applyFormReducer = (state, action={}) => {
   if (action.type !== blurType && action.type !== focusType) {
     return state;
   }
-  const fieldName = getIn(action, ['meta', 'field']);
-  const newFieldName = fieldName.substr(fieldName.indexOf('.') + 1);
-  return updateIn(state, ['values', fieldName], transformFieldEntry(newFieldName, action.type));
+
+  const nestedField = getIn(action, ['meta', 'field']);
+  const {index, field} = stripFieldNamePrefix(nestedField);
+  return updateIn(state, ['values', 'applications', index, field], transformFieldEntry(field, action.type));
 };
 
 export default combineReducers({
@@ -24,4 +38,5 @@ export default combineReducers({
   form: formReducer.plugin({
     apply: applyFormReducer,
   }),
+  selectedApplicant: selectedApplicantReducer.build(), // Creates a reducer function out of the Simr reducer
 });
